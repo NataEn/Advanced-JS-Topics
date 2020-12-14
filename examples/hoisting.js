ba863f4e6391907f707068fdcf4ffc2be7f04f83
natalie <natalie@enkin.co.il>
2020-12-14 12:30:11 +0200

    updating function hoisting

---
+++ examples/hoisting.js
@@ -0,0 +1,15 @@
+console.log(printName1("max"));
+/*
+All next calls will return a ReferenceError, since they are not yet defined
+console.log(printName2("max")); 
+console.log(printName2); 
+console.log(printName3("max"));
+*/
+function printName1(name) {
+  return name;
+}
+
+const printName2 = function (name) {
+  return name;
+};
+const printName3 = (name) => name;
