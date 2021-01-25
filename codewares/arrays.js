d68801024f47b8643e977b75ddf6790ff5d643d5
natalie <natalie@enkin.co.il>
2021-01-25 22:54:23 +0200

    updating question 2 in arrays

--- codewares/arrays.js
+++ codewares/arrays.js
@@ -20,6 +20,13 @@ function findUniq(arr) {
     return sortedArr[0];
   }
 }
+// There is an array of strings. All strings contains similar letters except one. Try to find it!
+
+// findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]) === 'BbBb'
+// findUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]) === 'foo'
+// Strings may contain spaces. Spaces is not significant, only non-spaces symbols matters. E.g. string that contains only spaces is like empty string.
+
+// It’s guaranteed that array contains more than 3 strings.
 function findUniqStr(arr) {
   for (let i = 0; i < arr.length - 3; i += 3) {
     const firstLetter = arr[i].toLowerCase().split().sort()[0];
