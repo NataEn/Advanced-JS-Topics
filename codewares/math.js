983e8bb1bd4840e329bd7a75e9725d803c54bfb6
natalie <natalie@enkin.co.il>
2021-01-28 19:25:46 +0200

    string kata

--- codewares/math.js
+++ codewares/math.js
@@ -1,13 +1,5 @@
 // Get decimal part of the given number
-// 8081% of 72208Anvar Turobov
-// JavaScript
-// TRAINNEXT KATA
-// Details
-// Solutions
-// Discourse (57)
-// Collect|
 // Write a function getDecimal(n) that returns only the decimal part of the given number.
-
 // You only have to handle valid numbers, not Infinity, NaN, or similar. Always return a positive decimal part.
 
 // Examples
--- codewares/strings.js
+++ codewares/strings.js
@@ -20,3 +20,17 @@ function highAndLow(numbers) {
   const max = Math.max(...stringArr);
   return `${max} ${min}`;
 }
+// Two to One
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+// Examples:
+// a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+// a = "abcdefghijklmnopqrstuvwxyz"
+// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+function longest(s1, s2) {
+  const total_string = s1 + s2;
+  return [...new Set(total_string.split(""))].sort().join("");
+}
