c3c052eb98c7a6c3d0faee4faad277db4e7bf2ad
natalie <natalie@enkin.co.il>
2021-01-28 19:00:46 +0200

    math problems file

--- codewares/BouncingBeams.js
+++ codewares/BouncingBeams.js
@@ -37,3 +37,4 @@ function explode(x) {
   }
 }
 explode(["a", 3]);
+
---
+++ codewares/math.js
@@ -0,0 +1,19 @@
+// Get decimal part of the given number
+// 8081% of 72208Anvar Turobov
+// JavaScript
+// TRAINNEXT KATA
+// Details
+// Solutions
+// Discourse (57)
+// Collect|
+// Write a function getDecimal(n) that returns only the decimal part of the given number.
+
+// You only have to handle valid numbers, not Infinity, NaN, or similar. Always return a positive decimal part.
+
+// Examples
+// getDecimal(2.4)  === 0.4
+// getDecimal(-0.2) === 0.2
+
+function getDecimal(n) {
+  return n; // fix me
+}
